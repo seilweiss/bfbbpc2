1e51c40f2c677456734601ccd593a77befe532ef
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-20 17:06:29 -0600

    Stub zMusic.cpp

---
+++ src/Game/zMusic.cpp
@@ -0,0 +1,16 @@
+#include "zMusic.h"
+
+static void volume_reset() STUB_VOID
+void zMusicRefreshVolume() STUB_VOID
+void zMusicInit() STUB_VOID
+static int32 getCurrLevelMusicEnum() STUB
+static bool32 zMusicDo(int32 track) STUB
+void zMusicNotify(int32 situation) STUB_VOID
+void zMusicNotifyEvent(const float32* toParam, xBase*) STUB_VOID
+void zMusicUpdate(float32 dt) STUB_VOID
+static void volume_update(float32) STUB_VOID
+void zMusicKill() STUB_VOID
+void zMusicPause() STUB_VOID
+void zMusicUnpause(bool32 kill) STUB_VOID
+void zMusicSetVolume(float32 vol, float32 delay) STUB_VOID
+void zMusicReset() STUB_VOID
--- src/Game/zMusic.h
+++ src/Game/zMusic.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <types.h>
+
+struct xBase;
+
+void zMusicRefreshVolume();
+void zMusicInit();
+void zMusicNotify(int32 situation);
+void zMusicNotifyEvent(const float32* toParam, xBase*);
+void zMusicUpdate(float32 dt);
+void zMusicKill();
+void zMusicPause();
+void zMusicUnpause(bool32 kill);
+void zMusicSetVolume(float32 vol, float32 delay);
+void zMusicReset();
