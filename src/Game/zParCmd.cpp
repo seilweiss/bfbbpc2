b4f7087a5a9aa0a9d9bac1a140800cb75bca8399
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-20 17:11:57 -0600

    Stub zParCmd.cpp

---
+++ src/Game/zParCmd.cpp
@@ -0,0 +1,13 @@
+#include "zParCmd.h"
+
+void zParCmdInit() STUB_VOID
+void zParCmdFindClipVolumes() STUB_VOID
+void xParCmdKillDistance_Update(xParCmd* c, xParGroup* ps, float32) STUB_VOID
+void xParCmdClipVolumes_Update(xParCmd*, xParGroup* ps, float32) STUB_VOID
+void xParCmdPlayerCollision_Update(xParCmd*, xParGroup*, float32) STUB_VOID
+void xParCmdAnimalMagentism_Update(xParCmd* c, xParGroup* ps, float32 dt) STUB_VOID
+void xParCmdDamagePlayer_Update(xParCmd* c, xParGroup* ps, float32) STUB_VOID
+void xParCmdJet_Update(xParCmd* c, xParGroup* ps, float32 dt) STUB_VOID
+void xParCmdCustom_Grass_Update(xParCmd*, xParGroup* ps, float32 dt) STUB_VOID
+void xParCmdApplyCamMat_Update(xParCmd* c, xParGroup* ps, float32 dt) STUB_VOID
+void xParCmdCustom_Update(xParCmd* c, xParGroup* ps, float32 dt) STUB_VOID
--- src/Game/zParCmd.h
+++ src/Game/zParCmd.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "xParCmd.h"
+
+void zParCmdInit();
+void zParCmdFindClipVolumes();
+void xParCmdKillDistance_Update(xParCmd* c, xParGroup* ps, float32);
+void xParCmdClipVolumes_Update(xParCmd*, xParGroup* ps, float32);
+void xParCmdPlayerCollision_Update(xParCmd*, xParGroup*, float32);
+void xParCmdAnimalMagentism_Update(xParCmd* c, xParGroup* ps, float32 dt);
+void xParCmdDamagePlayer_Update(xParCmd* c, xParGroup* ps, float32);
+void xParCmdJet_Update(xParCmd* c, xParGroup* ps, float32 dt);
+void xParCmdCustom_Grass_Update(xParCmd*, xParGroup* ps, float32 dt);
+void xParCmdApplyCamMat_Update(xParCmd* c, xParGroup* ps, float32 dt);
+void xParCmdCustom_Update(xParCmd* c, xParGroup* ps, float32 dt);
