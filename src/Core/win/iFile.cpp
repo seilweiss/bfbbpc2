797e05de38861ad66dac0a41b769c51d35dbf8b5
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-20 22:23:49 -0600

    Stub iFile.cpp

--- src/Core/win/iFile.cpp
+++ src/Core/win/iFile.cpp
@@ -1,3 +1,22 @@
 #include "iFile.h"
 
+#include "xFile.h"
+
+void iFileInit() STUB_VOID
+void iFileExit() {}
+uint32* iFileLoad(char* name, uint32* buffer, uint32* size) STUB
+uint32 iFileOpen(const char* name, int32 flags, tag_xFile* file) STUB
+int32 iFileSeek(xFile* file, int32 offset, int32 whence) STUB
+static void ifilereadCB(xFile*) STUB_VOID
+uint32 iFileRead(tag_xFile* file, void* buf, uint32 size) STUB
+// GameCube only
+//static void async_cb(long32, DVDFileInfo*) STUB_VOID
+int32 iFileReadAsync(xFile*, void*, uint32, iFileReadAsyncCallback, int32) STUB
 IFILE_READSECTOR_STATUS iFileReadAsyncStatus(int32 key, int32* amtToFar) STUB
+uint32 iFileClose(xFile* file) STUB
+uint32 iFileGetSize(xFile* file) STUB
+void iFileReadStop() STUB_VOID
+void iFileFullPath(const char* relname, char* fullname) STUB_VOID
+void iFileSetPath(char* path) STUB_VOID
+uint32 iFileFind(const char*, tag_xFile*) STUB
+void iFileGetInfo(tag_xFile*, uint32*, uint32*) STUB_VOID
--- src/Core/win/iFile.h
+++ src/Core/win/iFile.h
@@ -2,6 +2,8 @@
 
 #include <types.h>
 
+struct tag_xFile;
+
 enum IFILE_READSECTOR_STATUS
 {
 	IFILE_RDSTAT_NOOP,
@@ -17,6 +19,22 @@ typedef struct tag_iFile
 
 } iFile;
 
+typedef void(*iFileReadAsyncCallback)(tag_xFile*);
+
+void iFileInit();
+void iFileExit();
+uint32* iFileLoad(char* name, uint32* buffer, uint32* size);
+uint32 iFileOpen(const char* name, int32 flags, tag_xFile* file);
+int32 iFileSeek(tag_xFile* file, int32 offset, int32 whence);
+uint32 iFileRead(tag_xFile* file, void* buf, uint32 size);
+int32 iFileReadAsync(tag_xFile*, void*, uint32, iFileReadAsyncCallback, int32);
 IFILE_READSECTOR_STATUS iFileReadAsyncStatus(int32 key, int32* amtToFar);
+uint32 iFileClose(tag_xFile* file);
+uint32 iFileGetSize(tag_xFile* file);
+void iFileReadStop();
+void iFileFullPath(const char* relname, char* fullname);
+void iFileSetPath(char* path);
+uint32 iFileFind(const char*, tag_xFile*);
+void iFileGetInfo(tag_xFile*, uint32*, uint32*);
 
 inline void iFileAsyncService() {}
