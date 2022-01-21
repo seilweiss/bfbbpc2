a65f484d82378978173294f5b8d7c374a03ac575
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-21 00:46:29 -0600

    Stub iSystem.cpp

---
+++ src/Core/win/iSystem.cpp
@@ -0,0 +1,16 @@
+#include "iSystem.h"
+
+void iVSync() STUB_VOID
+static void TRCInit() STUB_VOID
+void iSystemInit(uint32 options) STUB_VOID
+void iSystemExit() STUB_VOID
+static uint32 RWAttachPlugins() STUB
+static uint32 RenderWareInit() STUB
+static void RenderWareExit() STUB_VOID
+static RwTexture* TextureRead(const char* name, const char* maskName) STUB
+uint8 iGetMinute() STUB
+uint8 iGetHour() STUB
+uint8 iGetDay() STUB
+uint8 iGetMonth() STUB
+uint32 iGetCurrFormattedDate(char* str) STUB
+uint32 iGetCurrFormattedTime(char* str) STUB
--- src/Core/win/iSystem.h
+++ src/Core/win/iSystem.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <types.h>
+#include <rwcore.h>
+
+void iVSync();
+void iSystemInit(uint32 options);
+void iSystemExit();
+uint8 iGetMinute();
+uint8 iGetHour();
+uint8 iGetDay();
+uint8 iGetMonth();
+uint32 iGetCurrFormattedDate(char* str);
+uint32 iGetCurrFormattedTime(char* str);
