d96fb68757e879abb4065657415159d09698efaf
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-20 22:33:14 -0600

    Stub iMath.cpp

---
+++ src/Core/win/iMath.cpp
@@ -0,0 +1,5 @@
+#include "iMath.h"
+
+float32 isin(float32) STUB
+float32 icos(float32) STUB
+float32 itan(float32) STUB
--- src/Core/win/iMath.h
+++ src/Core/win/iMath.h
@@ -4,3 +4,7 @@
 #include <math.h>
 
 #define iabs(x) (float32)fabs((x))
+
+float32 isin(float32);
+float32 icos(float32);
+float32 itan(float32);
