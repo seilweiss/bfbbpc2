d8e5c99b074ce6af27f76df1f12cfa74d3613714
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-20 22:27:46 -0600

    Stub iFMV.cpp

---
+++ src/Core/win/iFMV.cpp
@@ -0,0 +1,3 @@
+#include "iFMV.h"
+
+uint32 iFMVPlay(char* filename, uint32 buttons, float32 time, bool, bool) STUB
--- src/Core/win/iFMV.h
+++ src/Core/win/iFMV.h
@@ -1,1 +1,5 @@
 #pragma once
+
+#include <types.h>
+
+uint32 iFMVPlay(char* filename, uint32 buttons, float32 time, bool, bool);
