581557c8e2580b840c512655eee73fa81e20ce6c
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-20 21:31:05 -0600

    Stub iAnim.cpp

---
+++ src/Core/win/iAnim.cpp
@@ -0,0 +1,7 @@
+#include "iAnim.h"
+
+void iAnimInit() STUB_VOID
+void iAnimEval(void* RawData, float32 time, uint32 flags, xVec3* tran, xQuat* quat) STUB_VOID
+float32 iAnimDuration(void* RawData) STUB
+uint32 iAnimBoneCount(void* RawData) STUB
+void iAnimBlend(float32 BlendFactor, float32 BlendRecip, uint16* BlendTimeOffset, float32* BoneTable, uint32 BoneCount, xVec3* Tran1, xQuat* Quat1, xVec3* Tran2, xQuat* Quat2, xVec3* TranDest, xQuat* QuatDest) STUB_VOID
--- src/Core/win/iAnim.h
+++ src/Core/win/iAnim.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include "xMath3.h"
+
+void iAnimInit();
+void iAnimEval(void* RawData, float32 time, uint32 flags, xVec3* tran, xQuat* quat);
+float32 iAnimDuration(void* RawData);
+uint32 iAnimBoneCount(void* RawData);
+void iAnimBlend(float32 BlendFactor, float32 BlendRecip, uint16* BlendTimeOffset, float32* BoneTable, uint32 BoneCount, xVec3* Tran1, xQuat* Quat1, xVec3* Tran2, xQuat* Quat2, xVec3* TranDest, xQuat* QuatDest);
