94d0d1b3a66e2ece22d99468a4c1ec4f6398a834
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-22 01:26:06 -0600

    Stub zNPCGoalTiki.cpp

---
+++ src/Game/zNPCGoalTiki.cpp
@@ -0,0 +1,12 @@
+#include "zNPCGoalTiki.h"
+
+xFactoryInst* GOALCreate_Tiki(int32 who, RyzMemGrow* grow, void*) STUB
+bool32 zNPCGoalTikiIdle::Enter(float32, void*) STUB
+bool32 zNPCGoalTikiPatrol::Enter(float32, void*) STUB
+bool32 zNPCGoalTikiHide::Enter(float32, void*) STUB
+bool32 zNPCGoalTikiHide::Exit(float32, void*) STUB
+bool32 zNPCGoalTikiCount::Enter(float32, void*) STUB
+bool32 zNPCGoalTikiDying::Enter(float32, void*) STUB
+bool32 zNPCGoalTikiDying::Exit(float32, void*) STUB
+bool32 zNPCGoalTikiDead::Enter(float32, void*) STUB
+bool32 zNPCGoalTikiDead::Exit(float32, void*) STUB
--- src/Game/zNPCGoalTiki.h
+++ src/Game/zNPCGoalTiki.h
@@ -1,1 +1,80 @@
 #pragma once
+
+#include "zNPCGoalCommon.h"
+#include "xMath3.h"
+
+class zNPCGoalTikiIdle : public zNPCGoalCommon
+{
+private:
+	float32 tmr_wait;
+
+public:
+	zNPCGoalTikiIdle(int32 myType) WIP : zNPCGoalCommon(myType) {}
+
+	virtual void Clear() {}
+	virtual bool32 Enter(float32, void*);
+};
+
+class zNPCGoalTikiPatrol : public zNPCGoalCommon
+{
+private:
+	xVec3 dest_pos;
+	xVec3 vel;
+
+public:
+	zNPCGoalTikiPatrol(int32 myType) WIP : zNPCGoalCommon(myType) {}
+
+	virtual void Clear() {}
+	virtual bool32 Enter(float32, void*);
+};
+
+class zNPCGoalTikiHide : public zNPCGoalCommon
+{
+public:
+	zNPCGoalTikiHide(int32 myType) WIP : zNPCGoalCommon(myType) {}
+
+	virtual void Clear() {}
+	virtual bool32 Enter(float32, void*);
+	virtual bool32 Exit(float32, void*);
+};
+
+class zNPCGoalTikiCount : public zNPCGoalCommon
+{
+private:
+	float32 tmr_count;
+	int32 beingCarried;
+
+public:
+	zNPCGoalTikiCount(int32 myType) WIP : zNPCGoalCommon(myType) {}
+
+	virtual void Clear() {}
+	virtual bool32 Enter(float32, void*);
+};
+
+class zNPCGoalTikiDying : public zNPCGoalCommon
+{
+private:
+	float32 tmr_dying;
+
+public:
+	zNPCGoalTikiDying(int32 myType) WIP : zNPCGoalCommon(myType) {}
+
+	virtual void Clear() {}
+	virtual bool32 Enter(float32, void*);
+	virtual bool32 Exit(float32, void*);
+};
+
+class zNPCGoalTikiDead : public zNPCGoalCommon
+{
+private:
+	float32 tmr_resurrect;
+
+public:
+	zNPCGoalTikiDead(int32 myType) WIP : zNPCGoalCommon(myType) {}
+
+	virtual void Clear() {}
+	virtual bool32 Enter(float32, void*);
+	virtual bool32 Exit(float32, void*);
+};
+
+xFactoryInst* GOALCreate_Tiki(int32 who, RyzMemGrow* grow, void*);
