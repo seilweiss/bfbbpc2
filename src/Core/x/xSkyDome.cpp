4a174f588c5e0d9d46f2654939ce67ca551d8290
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-22 23:21:58 -0600

    Stub xSkyDome.cpp

---
+++ src/Core/x/xSkyDome.cpp
@@ -0,0 +1,6 @@
+#include "xSkyDome.h"
+
+static void xSkyDome_EmptyRender(xEnt*) {}
+void xSkyDome_Setup() STUB_VOID
+void xSkyDome_AddEntity(xEnt* ent, int32 sortorder, bool32 lockY) STUB_VOID
+void xSkyDome_Render() STUB_VOID
--- src/Core/x/xSkyDome.h
+++ src/Core/x/xSkyDome.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#include "xEnt.h"
+
+void xSkyDome_Setup();
+void xSkyDome_AddEntity(xEnt* ent, int32 sortorder, bool32 lockY);
+void xSkyDome_Render();
