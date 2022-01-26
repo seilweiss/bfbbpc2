3f4b415d1b1913cf0be25aafca94fd4fe7208c42
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-26 16:52:02 -0600

    Stub xRenderState.cpp

---
+++ src/Core/x/xRenderState.cpp
@@ -0,0 +1,5 @@
+#include "xRenderState.h"
+
+void xRenderStateSetTexture(RwTexture* texture) STUB_VOID
+void xRenderStateSetSrcBlendMode(int32 xmode) STUB_VOID
+void xRenderStateSetDstBlendMode(int32 xmode) STUB_VOID
--- src/Core/x/xRenderState.h
+++ src/Core/x/xRenderState.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include <types.h>
+#include <rwcore.h>
+
+void xRenderStateSetTexture(RwTexture* texture);
+void xRenderStateSetSrcBlendMode(int32 xmode);
+void xRenderStateSetDstBlendMode(int32 xmode);
