a3cb022ae36fec3bd73eb4bca890e636abd70c09
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-26 15:53:27 -0600

    Stub zCollGeom.cpp

---
+++ src/Game/zCollGeom.cpp
@@ -0,0 +1,6 @@
+#include "zCollGeom.h"
+
+uint32 zCollGeom_EntSetup(xEnt* ent) STUB
+void zCollGeom_Init() STUB_VOID
+void zCollGeom_CamEnable(xEnt* ent) STUB_VOID
+void zCollGeom_CamDisable(xEnt* ent) STUB_VOID
--- src/Game/zCollGeom.h
+++ src/Game/zCollGeom.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include "xEnt.h"
+
+uint32 zCollGeom_EntSetup(xEnt* ent);
+void zCollGeom_Init();
+void zCollGeom_CamEnable(xEnt* ent);
+void zCollGeom_CamDisable(xEnt* ent);
