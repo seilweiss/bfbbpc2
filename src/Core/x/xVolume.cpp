f9221d0f811054fcdcbc003f7e41e2565b57faf8
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-26 16:38:22 -0600

    Stub xVolume.cpp

---
+++ src/Core/x/xVolume.cpp
@@ -0,0 +1,7 @@
+#include "xVolume.h"
+
+void xVolume::Init(xVolumeAsset* asset) STUB_VOID
+void xVolume::Reset() STUB_VOID
+void xVolume::Save(xSerial* s) STUB_VOID
+void xVolume::Load(xSerial* s) STUB_VOID
+xBound* xVolume::GetBound() STUB
--- src/Core/x/xVolume.h
+++ src/Core/x/xVolume.h
@@ -15,4 +15,10 @@ struct xVolumeAsset : xBaseAsset
 struct xVolume : xBase
 {
 	xVolumeAsset* asset;
+
+	void Init(xVolumeAsset* asset);
+	void Reset();
+	void Save(xSerial* s);
+	void Load(xSerial* s);
+	xBound* GetBound();
 };
