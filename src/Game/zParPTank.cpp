9f9f1e664acf8453700617be7d3c6d41afc9c702
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-26 22:16:30 -0600

    Stub zParPTank.cpp

---
+++ src/Game/zParPTank.cpp
@@ -0,0 +1,39 @@
+#include "zParPTank.h"
+
+namespace {
+
+	struct snow_particle_data
+	{
+		xVec3 loc;
+		float32 size;
+		xVec3 vel;
+		float32 life;
+		float32 u;
+		float32 pad[3];
+	};
+
+}
+
+static void zParPTankSparkleCreate(zParPTank* zp, uint32 max_particles, zParPTankUpdateCallback update) STUB_VOID
+static void zParPTankSparkleUpdate(zParPTank* zp, float32 dt) STUB_VOID
+void zParPTankSpawnSparkles(xVec3* pos, uint32 count) STUB_VOID
+static void zParPTankBubbleCreate(zParPTank* zp, uint32 max_particles, zParPTankUpdateCallback update) STUB_VOID
+static void zParPTankMenuBubbleCreate(zParPTank* zp, uint32 max_particles, zParPTankUpdateCallback update) STUB_VOID
+static void zParPTankBubbleUpdate(zParPTank* zp, float32 dt) STUB_VOID
+static void zParPTankSpawnBubbles(xVec3* pos, xVec3* vel, uint32 count, float32 scale, zParPTank* zp) STUB_VOID
+void zParPTankSpawnBubbles(xVec3* pos, xVec3* vel, uint32 count, float32 scale) STUB_VOID
+int32 zParPTankBubblesAvailable() STUB
+void zParPTankSpawnMenuBubbles(xVec3* pos, xVec3* vel, uint32 count) STUB_VOID
+static void zParPTankSnowCreate(zParPTank*, uint32, zParPTankUpdateCallback) STUB_VOID
+static void zParPTankSnowUpdate(zParPTank* zp, float32 dt) STUB_VOID
+void zParPTankSpawnSnow(xVec3* pos, xVec3* vel, uint32 count) STUB_VOID
+static void zParPTankSteamCreate(zParPTank* zp, uint32 max_particles, zParPTankUpdateCallback update) STUB_VOID
+static void zParPTankSteamUpdate(zParPTank* zp, float32 dt) STUB_VOID
+zParPTank* zParPTankAdd() STUB
+void zParPTankInit() STUB_VOID
+void zParPTankSceneEnter() STUB_VOID
+void zParPTankSceneExit() {}
+void zParPTankExit() STUB_VOID
+void zParPTankUpdate(float32 dt) STUB_VOID
+void zParPTankRender() STUB_VOID
+int32 zParPTankConvertEmitRate(xParEmitter* pe, float32 dt) STUB
--- src/Game/zParPTank.h
+++ src/Game/zParPTank.h
@@ -1,1 +1,34 @@
 #pragma once
+
+#include "xMath3.h"
+
+#include <rwcore.h>
+#include <rpworld.h>
+
+struct zParPTank;
+struct xParEmitter;
+
+typedef void(*zParPTankUpdateCallback)(zParPTank*, float32);
+
+struct zParPTank
+{
+	uint32 flags;
+	zParPTankUpdateCallback update;
+	RpAtomic* ptank;
+	uint32 num_particles;
+	uint32 max_particles;
+};
+
+void zParPTankSpawnSparkles(xVec3* pos, uint32 count);
+void zParPTankSpawnBubbles(xVec3* pos, xVec3* vel, uint32 count, float32 scale);
+int32 zParPTankBubblesAvailable();
+void zParPTankSpawnMenuBubbles(xVec3* pos, xVec3* vel, uint32 count);
+void zParPTankSpawnSnow(xVec3* pos, xVec3* vel, uint32 count);
+zParPTank* zParPTankAdd();
+void zParPTankInit();
+void zParPTankSceneEnter();
+void zParPTankSceneExit();
+void zParPTankExit();
+void zParPTankUpdate(float32 dt);
+void zParPTankRender();
+int32 zParPTankConvertEmitRate(xParEmitter* pe, float32 dt);
