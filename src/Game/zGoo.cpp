e6fc167b205e4749392f7fcea8a9d9d824088db6
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-26 19:44:43 -0600

    Stub zGoo.cpp

---
+++ src/Game/zGoo.cpp
@@ -0,0 +1,10 @@
+#include "zGoo.h"
+
+void zGooInit(int32 nobj) STUB_VOID
+void zGooExit() STUB_VOID
+bool32 zGooAdd(xEnt* obj, float32 depth, int32 freezeGroup) STUB
+bool32 zGooIs(xEnt* obj, float32& depth, uint32 playerCheck) STUB
+void zGooCollsBegin() STUB_VOID
+void zGooCollsEnd() STUB_VOID
+void zGooStopTide() STUB_VOID
+void zGooMeltFinished(RpAtomic* atomic) STUB_VOID
--- src/Game/zGoo.h
+++ src/Game/zGoo.h
@@ -1,7 +1,18 @@
 #pragma once
 
 #include <types.h>
+#include <rwcore.h>
+#include <rpworld.h>
 
 struct xEnt;
 
-bool32 zGooIs(xEnt*) STUB
+void zGooInit(int32 nobj);
+void zGooExit();
+bool32 zGooAdd(xEnt* obj, float32 depth, int32 freezeGroup);
+bool32 zGooIs(xEnt* obj, float32& depth, uint32 playerCheck);
+void zGooCollsBegin();
+void zGooCollsEnd();
+void zGooStopTide();
+void zGooMeltFinished(RpAtomic* atomic);
+
+inline bool32 zGooIs(xEnt*) STUB
