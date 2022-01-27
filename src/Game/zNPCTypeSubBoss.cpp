c665ee81a44f7f1dc96daf5801d3ba01ec13fb03
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-26 21:42:18 -0600

    Stub zNPCTypeSubBoss.cpp

---
+++ src/Game/zNPCTypeSubBoss.cpp
@@ -0,0 +1,12 @@
+#include "zNPCTypeSubBoss.h"
+
+void ZNPC_SubBoss_Startup() STUB_VOID
+void ZNPC_SubBoss_Shutdown() {}
+void zNPCSubBoss_ScenePrepare() STUB_VOID
+void zNPCSubBoss_SceneFinish() STUB_VOID
+xFactoryInst* ZNPC_Create_SubBoss(int32 who, RyzMemGrow* grow, void*) STUB
+void ZNPC_Destroy_SubBoss(xFactoryInst* inst) STUB_VOID
+
+void zNPCSubBoss::Setup() STUB_VOID
+
+void SUBB_InitEffects() STUB_VOID
--- src/Game/zNPCTypeSubBoss.h
+++ src/Game/zNPCTypeSubBoss.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include "zNPCTypeCommon.h"
+#include "zNPCSupport.h"
+
+class zNPCSubBoss : public zNPCCommon
+{
+private:
+	NPCTarget tgt_cur;
+
+public:
+	zNPCSubBoss(int32 myType) WIP : zNPCCommon(myType) {}
+
+	virtual void Setup();
+	virtual uint8 ColChkFlags() const WIP { return 0; }
+	virtual uint8 ColPenFlags() const WIP { return 0; }
+	virtual uint8 ColChkByFlags() const WIP { return 0x10; }
+	virtual uint8 ColPenByFlags() const WIP { return 0x10; }
+	virtual uint8 PhysicsFlags() const WIP { return 0x3; }
+};
+
+void ZNPC_SubBoss_Startup();
+void ZNPC_SubBoss_Shutdown();
+void zNPCSubBoss_ScenePrepare();
+void zNPCSubBoss_SceneFinish();
+xFactoryInst* ZNPC_Create_SubBoss(int32 who, RyzMemGrow* grow, void*);
+void ZNPC_Destroy_SubBoss(xFactoryInst* inst);
+void SUBB_InitEffects();
