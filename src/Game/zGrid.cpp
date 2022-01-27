bd67410985ad2d10aa45e552131c2186ffc86df0
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-26 19:46:16 -0600

    Stub zGrid.cpp

---
+++ src/Game/zGrid.cpp
@@ -0,0 +1,7 @@
+#include "zGrid.h"
+
+static void hack_flag_shadows(zScene* s) STUB_VOID
+void zGridReset(zScene* s) STUB_VOID
+void zGridInit(zScene* s) STUB_VOID
+void zGridExit(zScene*) STUB_VOID
+void zGridUpdateEnt(xEnt* ent) STUB_VOID
--- src/Game/zGrid.h
+++ src/Game/zGrid.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "xGrid.h"
+
+struct zScene;
+
+void zGridReset(zScene* s);
+void zGridInit(zScene* s);
+void zGridExit(zScene*);
+void zGridUpdateEnt(xEnt* ent);
