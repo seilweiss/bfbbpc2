092757daf4bedb9701bc6f9a9cfe5f756561086a
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-26 20:52:51 -0600

    Stub zNPCGoalDuplotron.cpp

---
+++ src/Game/zNPCGoalDuplotron.cpp
@@ -0,0 +1,11 @@
+#include "zNPCGoalDuplotron.h"
+
+xFactoryInst* GOALCreate_Duplotron(int32 who, RyzMemGrow* grow, void*) STUB
+
+bool32 zNPCGoalDuploLive::Enter(float32 dt, void* updCtxt) STUB
+bool32 zNPCGoalDuploLive::Process(en_trantype* trantype, float32 dt, void* updCtxt, xScene* xscn) STUB
+void zNPCGoalDuploLive::CheckPlayer() STUB_VOID
+bool32 zNPCGoalDuploLive::NPCMessage(NPCMsg* mail) STUB
+void zNPCGoalDuploLive::SDS_BigRedButton() STUB_VOID
+bool32 zNPCGoalDuploLive::SDS_Countdown(float32 dt) STUB
+void zNPCGoalDuploLive::SDS_StartExplode() STUB_VOID
--- src/Game/zNPCGoalDuplotron.h
+++ src/Game/zNPCGoalDuplotron.h
@@ -1,1 +1,44 @@
 #pragma once
+
+#include "zNPCGoalCommon.h"
+#include "zNPCGoalStd.h"
+
+enum en_dupolive
+{
+	LIVESTAT_NORMAL,
+	LIVESTAT_COUNTDOWN
+};
+
+class zNPCGoalDuploLive : public zNPCGoalCommon
+{
+private:
+	float32 tmr_chkPlyrDist;
+	int32 cnt_destruct;
+	en_dupolive livestat;
+
+public:
+	zNPCGoalDuploLive(int32 myType) WIP : zNPCGoalCommon(myType)
+	{
+		SetFlags(0xC);
+	}
+
+	virtual bool32 Enter(float32 dt, void* updCtxt);
+	virtual bool32 Process(en_trantype* trantype, float32 dt, void* updCtxt, xScene* xscn);
+	virtual bool32 NPCMessage(NPCMsg* mail);
+
+	void CheckPlayer();
+	void SDS_BigRedButton();
+	bool32 SDS_Countdown(float32 dt);
+	void SDS_StartExplode();
+};
+
+class zNPCGoalDuploDead : public zNPCGoalDead
+{
+public:
+	zNPCGoalDuploDead(int32 myType) WIP : zNPCGoalDead(myType)
+	{
+		SetFlags(0x8);
+	}
+};
+
+xFactoryInst* GOALCreate_Duplotron(int32 who, RyzMemGrow* grow, void*);
